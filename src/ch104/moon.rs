8bab1bc91de98628028fbe9035e16046cfe1aeee
YoungPhil <youngphil.gim@gmail.com>
2024-05-07 18:01:44 +0900

    Initial commit

---
+++ src/ch104/moon.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let moon = 384400.0;
+    let car = 80.0;
+    let train = 300.0;
+
+    println!("달까지 자동차로 {}일", moon / car / 24.0);
+    println!("달까지 기차로 {}일", moon / train / 24.0);
+}
