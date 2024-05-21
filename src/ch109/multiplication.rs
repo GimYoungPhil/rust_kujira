1fabc321879ad3aa9dfa891863a9209957a8a1d7
YoungPhil <youngphil.gim@gmail.com>
2024-05-21 11:51:29 +0900

    fixed

---
+++ src/ch109/multiplication.rs
@@ -0,0 +1,11 @@
+fn multiplication(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn main() {
+    let ex1 = multiplication(3, 5);
+    println!("3 x 5 = {}", ex1);
+
+    let ex2 = multiplication(8, 4);
+    println!("8 x 4 = {}", ex2);
+}
